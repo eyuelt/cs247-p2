d341a1b175e17f0c867901945067d4012b9377fa
eyuelt <eyueltessema@gmail.com>
2015-01-26 07:10:29 -0800

    Get pretty much everything on there

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+function p(text) {
+  console.log(text);
+}
+
+var windowWidth = $(window).width();
+var windowHeight = $(window).height();
+$('.page').width(windowWidth);
+$('.page').height(windowHeight);
